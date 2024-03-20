f9c9575aa9cbc57e08b51b64997c245cf5e3d911
river_yun <carf_yunxl@163.com>
2024-03-20 13:51:59 +0800

    [add] pull files

--- HFST_Widgets_MFC/Resource.h
+++ HFST_Widgets_MFC/Resource.h
@@ -12,14 +12,35 @@
 #define IDB_PNG_UI_NORMAL               132
 #define IDB_PNG4                        133
 #define IDB_PNG_UI_WINDOW_ICON          133
+#define IDB_PNG_BOY                     134
+#define IDB_PNG_CUTE                    135
+#define IDB_PNG_GIRL                    136
+#define IDB_PNG_MOM                     137
+#define IDI_ICON1                       138
+#define IDI_ICON2                       139
+#define IDI_ICON3                       140
+#define IDI_ICON4                       141
+#define IDD_DIALOG1                     142
+#define IDR_TOOLBAR1                    144
+#define IDC_LIST_LOG_BOX                1000
+#define IDC_LIST_DRAWER_CONTENT         1001
+#define IDC_LIST1                       1004
+#define ID_BUTTON_WANG                  32771
+#define ID_BUTTON_LINE                  32772
+#define ID_BUTTON_SHI                   32773
+#define ID_BUTTON_RECT                  32774
+#define ID_BUTTON_MAX                   32775
+#define ID_BUTTON_MIN                   32776
+#define ID_BUTTON32777                  32777
+#define ID_BUTTON_CLOSE                 32777
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        134
-#define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_RESOURCE_VALUE        146
+#define _APS_NEXT_COMMAND_VALUE         32778
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
