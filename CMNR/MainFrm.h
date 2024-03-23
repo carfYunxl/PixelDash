e572c9b57d329d8b1c19e72579b10d773ed5b458
carfYunxl <carf_yunxl@163.com>
2024-03-24 01:26:00 +0800

    [TMP] 删除多余的项目

---
+++ CMNR/MainFrm.h
@@ -0,0 +1,56 @@
+﻿
+// MainFrm.h: CMainFrame 类的接口
+//
+
+#pragma once
+#include "ChildView.h"
+
+class CMainFrame : public CFrameWndEx
+{
+	
+public:
+	CMainFrame() noexcept;
+protected: 
+	DECLARE_DYNAMIC(CMainFrame)
+
+// 特性
+protected:
+	//CSplitterWnd m_wndSplitter;
+public:
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual BOOL OnCreateClient(LPCREATESTRUCT lpcs, CCreateContext* pContext);
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+	virtual BOOL OnCmdMsg(UINT nID, int nCode, void* pExtra, AFX_CMDHANDLERINFO* pHandlerInfo);
+	virtual BOOL LoadFrame(UINT nIDResource, DWORD dwDefaultStyle = WS_OVERLAPPEDWINDOW | FWS_ADDTOTITLE, CWnd* pParentWnd = nullptr, CCreateContext* pContext = nullptr);
+
+// 实现
+public:
+	virtual ~CMainFrame();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:  // 控件条嵌入成员
+	CMFCMenuBar       m_wndMenuBar;
+	CMFCToolBar       m_wndToolBar;
+	CMFCStatusBar     m_wndStatusBar;
+	CMFCToolBarImages m_UserImages;
+	CChildView    m_wndView;
+
+// 生成的消息映射函数
+protected:
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+	afx_msg void OnSetFocus(CWnd *pOldWnd);
+	afx_msg void OnViewCustomize();
+	afx_msg LRESULT OnToolbarCreateNew(WPARAM wp, LPARAM lp);
+	DECLARE_MESSAGE_MAP()
+
+};
+
+
